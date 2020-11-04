d0dd72507bb92382b69c6c373ee35f247c26449c
Orsolya <orsolya.suto@ericsson.com>
2020-11-04 08:39:57 +0100

    connecting POST backend point with Angular frontend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -14,9 +14,9 @@ export class PostsService {
   }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').subscribe((postData) => {
-    this. posts = postData.posts;
-    this.postsUpdated.next([...this.posts]);
+    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts').subscribe((postData) => {
+      this.posts = postData.posts;
+      this.postsUpdated.next([...this.posts]);
     });
   }
 
@@ -26,7 +26,10 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }
