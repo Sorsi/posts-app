e291e8bab34bbec7edcbb77e3f0ec8d086048335
Orsolya <orsolya.suto@ericsson.com>
2020-11-09 16:06:04 +0100

    adding post with id

--- backend/app.js
+++ backend/app.js
@@ -33,9 +33,11 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added succesfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added succesfully',
+      postId: createdPost.id
+    });
   });
 });
 
